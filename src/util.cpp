483324f1e4f54744a026da2c690f24383dde7866
Patrick José Pereira <patrickelectric@gmail.com>
2020-03-21 20:57:51 -0300

    util: Remove share code functions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/util.cpp
+++ src/util.cpp
@@ -17,33 +17,6 @@ void sayHi() {
 
 #endif
 
-QString Util::sharedCode() const
-{
-#ifdef EMSCRIPTEN
-    sayHi();
-    QString code = emscripten::val::global("window")["location"]["search"].as<std::string>().c_str();
-#else
-    QString code = "";
-#endif
-    if(code.startsWith("?code=")) {
-        return QUrl::fromPercentEncoding(code.remove(0, 6).toLatin1());
-    }
-    return QUrl::fromPercentEncoding(code.toLatin1());
-}
-
-QString Util::createSharedCode(const QString& code, bool tiny) const
-{
-    if(!tiny) {
-        return "https://patrickelectric.work/qmlonline/?code=" + QString(QUrl::toPercentEncoding(code));
-    }
-
-    auto url = QString("https://patrickelectric.work/qmlonline/?code=") + code;
-    for(const auto item : _urlEncodeMap.toStdMap()) {
-        url.replace(item.first, item.second);
-    }
-    return url;
-}
-
 QString Util::code() const
 {
     return _code;
@@ -78,59 +51,6 @@ QObject* Util::qmlSingletonRegister(QQmlEngine* engine, QJSEngine* scriptEngine)
 Util* Util::self()
 {
     static Util* self = new Util();
-    self->setCode(
-R"(import QtQuick 2.0
-
-Rectangle {
-    id: root
-    anchors.fill: parent
-    color: "black"
-
-    Rectangle {
-        id: rect
-        width: 50
-        height: 50
-        color: "red"
-        anchors.top: root.top
-        anchors.left: root.left
-        anchors.margins: 10
-    }
-
-    states: [
-        State {
-            name: "small"
-            AnchorChanges {
-                target: rect
-                anchors.bottom: root.bottom
-                anchors.right: root.right
-            }
-            PropertyChanges {
-                target: rect
-                height: 50
-                width: 50
-            }
-        },
-        State {
-            name: "big"
-            AnchorChanges {
-                target: rect
-                anchors.bottom: undefined
-                anchors.right: undefined
-            }
-        }
-    ]
-
-    Timer {
-        running: true; repeat: true; interval: 1000;
-        onTriggered: root.state = root.state ===  "small" ?  "big" : "small"
-    }
-
-    transitions: Transition {
-        AnchorAnimation { duration: 400 }
-    }
-}
-)"
-    );
     return self;
 }
 
