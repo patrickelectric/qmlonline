bf67d5d31362ef13ce16ba4414410be22d7f47c1
Patrick José Pereira <patrickelectric@gmail.com>
2020-03-21 20:36:27 -0300

    remove examples and syntax
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 
-#include "examples.h"
 #include "util.h"
-#include "syntaxhighlighter.h"
 
 int main(int argc, char *argv[])
 {
-    qmlRegisterType<Examples>("Examples", 1, 0, "Examples");
-    qmlRegisterType<SyntaxHighlighter>("SyntaxHighlighter", 1, 0, "SyntaxHighlighter");
     qmlRegisterSingletonType<Util>("Util", 1, 0, "Util", Util::qmlSingletonRegister);
 
     QGuiApplication app(argc, argv);
