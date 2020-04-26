291c27a364e92b5108e911a01b2df32a83693922
Patrick José Pereira <patrickelectric@gmail.com>
2020-04-26 14:26:46 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,8 @@ int main(int argc, char *argv[])
     qmlRegisterSingletonType<Util>("Util", 1, 0, "Util", Util::qmlSingletonRegister);
 
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine appEngine(QUrl("qrc:/main.qml"));
-
     KirigamiPlugin::getInstance().registerTypes();
+    QQmlApplicationEngine appEngine(QUrl("qrc:/main.qml"));
 
     return app.exec();
 }
