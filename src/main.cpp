0555b5e67e5f16f668f07d2c61bdef5c23bcbdd2
Patrick José Pereira <patrickelectric@gmail.com>
2020-04-27 10:41:54 -0300

    upload for tomaz
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 
 #include "3rdparty/kirigami/src/kirigamiplugin.h"
 
-//Q_IMPORT_PLUGIN(KirigamiPlugin);
-
 int main(int argc, char *argv[])
 {
     qmlRegisterSingletonType<Util>("Util", 1, 0, "Util", Util::qmlSingletonRegister);
@@ -18,32 +16,6 @@ int main(int argc, char *argv[])
     KirigamiPlugin::getInstance().registerTypes();
     QQmlEngine engine;
     QQmlComponent component(&engine);
-
-/*
-        component.setData(
-R"(
-import QtQuick.Controls 2.12
-
-ApplicationWindow {
-    visible: true
-
-    menuBar: MenuBar {
-
-    }
-
-    Button {
-        z: 10000000
-        text: "potato"
-        anchors.verticalCenter: parent.verticalCenter
-        anchors.horizontalCenter: parent.horizontalCenter
-        onClicked: print("potato")
-    }
-}
-)"
-
-            , {});
-        component.create();
-        */
 
     std::unique_ptr<QObject> componentPtr;
     QObject::connect(Util::self(), &Util::codeChanged, [&componentPtr, &component]() {
@@ -58,9 +30,5 @@ ApplicationWindow {
         qDebug() << "LOADED DATA!";
     });
 
-    while(true) {
-        app.exec();
-        qDebug() << "spin!";
-    }
-    return 0;
+    return app.exec();;
 }
