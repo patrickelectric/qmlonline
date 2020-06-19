41fa9af9afa09229ca72aac01d1a0b70e650c8b2
Patrick José Pereira <patrickelectric@gmail.com>
2020-06-19 19:12:26 -0300

    main: Include and register kirigami
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 
 #include "util.h"
 
+#include "3rdparty/kirigami/src/kirigamiplugin.h"
+
 int main(int argc, char *argv[])
 {
     qmlRegisterSingletonType<Util>("Util", 1, 0, "Util", Util::qmlSingletonRegister);
 
     QGuiApplication app(argc, argv);
+    KirigamiPlugin::getInstance().registerTypes();
+
     QQmlApplicationEngine appEngine(QUrl("qrc:/main.qml"));
     return app.exec();
 }
