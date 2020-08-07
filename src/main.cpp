4274050891f6b10f573ceb9ca14f64a5bc421d43
Patrick José Pereira <patrickelectric@gmail.com>
2020-08-07 20:42:49 -0300

    main: Enable highdpi
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 {
     qmlRegisterSingletonType<Util>("Util", 1, 0, "Util", Util::qmlSingletonRegister);
 
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     KirigamiPlugin::getInstance().registerTypes();
 
