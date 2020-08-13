72c44539644979b38244f39cb81d364b3ab981e2
Patrick José Pereira <patrickelectric@gmail.com>
2020-08-12 23:56:31 -0300

    html: Use new example style
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- html/examples.js
+++ html/examples.js
@@ -1,92 +1,104 @@
 function getExamples() {
-    return [
-        {
-            name: "Anchor Animation",
-            file: "anchoranimation.qml"
-        },
-        {
-            name: "Anchor Changes",
-            file: "anchorchanges.qml"
-        },
-        {
-            name: "Animated Image",
-            file: "animatedimage.qml"
-        },
-        {
-            name: "Animated Sprite",
-            file: "animatedsprite.qml"
-        },
-        {
-            name: "Animation Easing Type",
-            file: "animation-easing-type.qml"
-        },
-        {
-            name: "Binding",
-            file: "binding.qml"
-        },
-        {
-            name: "Blend Image",
-            file: "blend.qml"
-        },
-        {
-            name: "Brightness and Contrast",
-            file: "brightnesscontrast.qml"
-        },
-        {
-            name: "Canvas Advanced",
-            file: "canvasadvanced.qml"
-        },
-        {
-            name: "Game 1",
-            file: "pong.qml"
-        },
-        {
-            name: "Game 2",
-            file: "snake.qml"
-        },
-        {
-            name: "GridLayout",
-            file: "gridlayout.qml"
-        },
-        {
-            name: "Json Model",
-            file: "jsonmodel.qml"
-        },
-        {
-            name: "ListModel/ListElement",
-            file: "listmodel.qml"
-        },
-        {
-            name: "Repeater",
-            file: "repeater.qml"
-        },
-        {
-            name: "Rotation Animation",
-            file: "rotationanimator.qml"
-        },
-        {
-            name: "Search/Regex match",
-            file: "regex-match.qml"
-        },
-        {
-            name: "Sequential Animation",
-            file: "sequentialanimation.qml"
-        },
-        {
-            name: "Shader Effect Pixelated",
-            file: "shadereffect-pixelated.qml"
-        },
-        {
-            name: "Shader Effect",
-            file: "shadereffect.qml"
-        },
-        {
-            name: "Taphandler",
-            file: "taphandler.qml"
-        },
-        {
-            name: "Text Format",
-            file: "textformat.qml"
-        },
-    ]
+    return {
+        Animation: [
+            {
+                name: "Anchor Animation",
+                file: "anchoranimation.qml"
+            },
+            {
+                name: "Anchor Changes",
+                file: "anchorchanges.qml"
+            },
+            {
+                name: "Animated Image",
+                file: "animatedimage.qml"
+            },
+            {
+                name: "Animated Sprite",
+                file: "animatedsprite.qml"
+            },
+            {
+                name: "Animation Easing Type",
+                file: "animation-easing-type.qml"
+            },
+            {
+                name: "Rotation Animation",
+                file: "rotationanimator.qml"
+            },
+            {
+                name: "Sequential Animation",
+                file: "sequentialanimation.qml"
+            },
+        ],
+        Games: [
+            {
+                name: "Game 1",
+                file: "pong.qml"
+            },
+            {
+                name: "Game 2",
+                file: "snake.qml"
+            },
+        ],
+        Image: [
+            {
+                name: "Blend Image",
+                file: "blend.qml"
+            },
+            {
+                name: "Brightness and Contrast",
+                file: "brightnesscontrast.qml"
+            },
+        ],
+        Model: [
+            {
+                name: "Json Model",
+                file: "jsonmodel.qml"
+            },
+            {
+                name: "ListModel/ListElement",
+                file: "listmodel.qml"
+            },
+        ],
+        Random: [
+            {
+                name: "Binding",
+                file: "binding.qml"
+            },
+            {
+                name: "Canvas Advanced",
+                file: "canvasadvanced.qml"
+            },
+            {
+                name: "GridLayout",
+                file: "gridlayout.qml"
+            },
+            {
+                name: "Repeater",
+                file: "repeater.qml"
+            },
+            {
+                name: "Search/Regex match",
+                file: "regex-match.qml"
+            },
+            {
+                name: "Taphandler",
+                file: "taphandler.qml"
+            },
+            {
+                name: "Text Format",
+                file: "textformat.qml"
+            },
+        ],
+        Shaders: [
+            {
+                name: "Shader Effect Pixelated",
+                file: "shadereffect-pixelated.qml"
+            },
+            {
+                name: "Shader Effect",
+                file: "shadereffect.qml"
+            },
+        ],
+    }
 }
