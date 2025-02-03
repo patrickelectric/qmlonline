d6af1fe7b02e68131a649d58f70f17941f32e201
Patrick José Pereira <patrickelectric@gmail.com>
2025-02-03 15:38:59 -0300

    qt6 working
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/util.h
+++ src/util.h
@@ -73,5 +73,25 @@ private:
      */
     Util() = default;
 
-    QString _code;
+    QString _code = R"(import QtQuick 2.7
+import QtQuick.Controls 2.3
+
+Rectangle {
+    color: "red"
+    anchors.fill: parent
+
+    Text {
+        text: "WEEEEEEEEEE"
+        font.pixelSize: 50
+        color: "white"
+        anchors.centerIn: parent
+        RotationAnimator on rotation {
+            running: true
+            loops: Animation.Infinite
+            from: 0
+            to: 360
+            duration: 1500
+        }
+    }
+})";
 };
